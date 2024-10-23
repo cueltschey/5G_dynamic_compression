f888a9c59e359a0016f2f4278715f83b415ba5e5
Charles Ueltschey <chaseuelt@gmail.com>
2024-10-23 15:34:45 -0500

    write to binary file

--- receiver/main.cpp
+++ receiver/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
+#include <fstream>
 #include "d_compression/channel.h"
 #include "d_compression/iq.h"
 #include "d_compression/bfp.h"
@@ -13,6 +14,8 @@
 int main() {
     d_compression::channel wireless_channel("10.45.1.2", 5201, true);
     iq_conv converter;
+    std::ofstream vid_file("out.mp4");
+
 
     compression_options comp_type;
     size_t blank_bytes = 0;
@@ -20,7 +23,7 @@ int main() {
     while (true) {
         std::vector<uint8_t> buffer = wireless_channel.recv();
 
-        // read and pop compression type
+        // Read and pop compression type
         comp_type = static_cast<compression_options>(buffer.front());
         buffer.erase(buffer.begin());
         blank_bytes = static_cast<size_t>(buffer.front());
@@ -31,36 +34,42 @@ int main() {
         lz77_compressor lz77;
         std::vector<uint8_t> intermediate;
         std::vector<srsran::cbf16_t> iqSamples;
+
         switch (comp_type) {
-          case NONE:
-            std::cout << "Decoding: No compression -> " << buffer.size() << std::endl;
-            converter.deserialize(buffer, iqSamples);
-            converter.from_iq(iqSamples, buffer, blank_bytes);
-            break;
-          case BFP:
-            std::cout << "Decoding: Block Floating Point -> " << buffer.size() << std::endl;
-            iqSamples = bfp.decompress(buffer);
-            converter.from_iq(iqSamples, buffer, blank_bytes);
-            break;
-          case RLE:
-            std::cout << "Decoding: Run Length Encoding -> " << buffer.size() << std::endl;
-            rle.decompress(buffer, intermediate);
-            buffer = intermediate;
-            converter.deserialize(buffer, iqSamples);
-            converter.from_iq(iqSamples, buffer, blank_bytes);
-            break;
-          case LZ77:
-            std::cout << "Decoding: Lempel Ziv 77 -> " << buffer.size() << std::endl;
-            lz77.decompress(buffer, intermediate);
-            buffer = intermediate;
-            converter.deserialize(buffer, iqSamples);
-            converter.from_iq(iqSamples, buffer, blank_bytes);
-            break;
-          default:
-            std::cout << "Bad compression type -> " << buffer.size() << std::endl;
-            break;
+            case NONE:
+                std::cout << "Decoding: No compression -> " << buffer.size() << std::endl;
+                converter.deserialize(buffer, iqSamples);
+                converter.from_iq(iqSamples, buffer, blank_bytes);
+                break;
+            case BFP:
+                std::cout << "Decoding: Block Floating Point -> " << buffer.size() << std::endl;
+                iqSamples = bfp.decompress(buffer);
+                converter.from_iq(iqSamples, buffer, blank_bytes);
+                break;
+            case RLE:
+                std::cout << "Decoding: Run Length Encoding -> " << buffer.size() << std::endl;
+                rle.decompress(buffer, intermediate);
+                buffer = intermediate;
+                converter.deserialize(buffer, iqSamples);
+                converter.from_iq(iqSamples, buffer, blank_bytes);
+                break;
+            case LZ77:
+                std::cout << "Decoding: Lempel Ziv 77 -> " << buffer.size() << std::endl;
+                lz77.decompress(buffer, intermediate);
+                buffer = intermediate;
+                converter.deserialize(buffer, iqSamples);
+                converter.from_iq(iqSamples, buffer, blank_bytes);
+                break;
+            default:
+                std::cout << "Bad compression type -> " << buffer.size() << std::endl;
+                continue; // Skip to the next iteration
+        }
+
+        for(const uint8_t val : buffer){
+          vid_file << val;
         }
     }
 
     return 0;
 }
+
