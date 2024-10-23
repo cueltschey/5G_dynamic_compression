3004f8c6bf64fa048619cd46cdccd758bceb6f1d
Charles Ueltschey <chaseuelt@gmail.com>
2024-10-23 17:20:30 -0500

    add torch install scripts

---
+++ install-torch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
+unzip libtorch-shared-with-deps-latest.zip
+rm -f libtorch-shared-with-deps-latest.zip
